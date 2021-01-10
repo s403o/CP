4ce2f5baf31aa97586f8afdb2a5d2ce593e9faf6
s403o <eslam.adel@protonmail.com>
2021-01-10 19:40:59 +0200

    my cp structure

---
+++ my_template.cpp
@@ -0,0 +1,28 @@
+/*
+ ____       __  __
+/ ___|  ___|  \/  | ___
+\___ \ / _ \ |\/| |/ _ \
+ ___) |  __/ |  | | (_) |
+|____/ \___|_|  |_|\___/
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define semo  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef long int li;
+#define pi 3.141592653
+
+void Test()
+{
+#ifndef ONLINE_JUDGE
+  freopen("in", "r", stdin);
+  freopen("out", "w", stdout);
+#endif
+}
+
+int main() 
+{
+  semo;
+}
+
