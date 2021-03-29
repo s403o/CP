984de4c624378292cfcc5947ac75859247ba3d58
Eslam Adel <eslam.adel@protonmail.com>
2021-03-29 19:26:37 +0200

    Update my_template.cpp

--- my_template.cpp
+++ my_template.cpp
@@ -1,28 +1,29 @@
-/*
- ____       __  __
-/ ___|  ___|  \/  | ___
-\___ \ / _ \ |\/| |/ _ \
- ___) |  __/ |  | | (_) |
-|____/ \___|_|  |_|\___/
+/*   _  _    ___ _____
+ ___| || |  / _ \___ /  ___
+/ __| || |_| | | ||_ \ / _ \
+\__ \__   _| |_| |__) | (_) |
+|___/  |_|  \___/____/ \___/
 
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define semo  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define fast  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 typedef long long ll;
-typedef long int li;
+typedef long int li;	
+typedef long double ld;
 #define pi 3.141592653
+#define endl '\n'
 
 void Test()
 {
 #ifndef ONLINE_JUDGE
   freopen("in", "r", stdin);
-  freopen("out", "w", stdout);
+  //freopen("out", "w", stdout);
 #endif
 }
 
-int main() 
+int main()
 {
-  semo;
+	fast;
+	Test();
 }
-
