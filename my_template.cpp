188c48d8a9fc85e59da31dae739641aa46127421
Eslam Adel <eslam.adel.me@gmail.com>
2023-10-05 18:14:56 +0300

    updates

--- my_template.cpp
+++ my_template.cpp
@@ -1,24 +1,30 @@
-/*   _  _    ___ _____
- ___| || |  / _ \___ /  ___
-/ __| || |_| | | ||_ \ / _ \
-\__ \__   _| |_| |__) | (_) |
-|___/  |_|  \___/____/ \___/
-
+/*             ▄▄▄     ▄▄▄▄     ▄▄▄▄▄
+			  ▄███    ██▀▀██   █▀▀▀▀██▄
+ ▄▄█████▄    █▀ ██   ██    ██       ▄██   ▄████▄
+ ██▄▄▄▄ ▀  ▄█▀  ██   ██ ██ ██    █████   ██▀  ▀██
+  ▀▀▀▀██▄  ████████  ██    ██       ▀██  ██    ██
+ █▄▄▄▄▄██       ██    ██▄▄██   █▄▄▄▄██▀  ▀██▄▄██▀
+ ▀▀▀▀▀▀         ▀▀     ▀▀▀▀     ▀▀▀▀▀      ▀▀▀▀
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define fast  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define fast                          \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);                    \
+	cout.tie(NULL);
 typedef long long ll;
-typedef long int li;	
+typedef long int li;
 typedef long double ld;
+constexpr int MOD = 1000000007;
 #define pi 3.141592653
 #define endl '\n'
+#define all(v) v.begin(), v.end()
 
 void Test()
 {
 #ifndef ONLINE_JUDGE
-  freopen("in", "r", stdin);
-  //freopen("out", "w", stdout);
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
 #endif
 }
 
@@ -26,6 +32,4 @@ int main()
 {
 	fast;
 	Test();
-	
-	return 0;
 }
